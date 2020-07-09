63d32da80581020edf888685a5df4a7609078a93
Nghia <huunghia160799@gmail.com>
2020-07-09 11:02:19 +0700

    add multithreading to find fork

--- fork-detection/fork_detection.cpp
+++ fork-detection/fork_detection.cpp
@@ -70,13 +70,13 @@ public:
 // }
 
 std::vector<uint64_t>snapshots;
-
 const uint64_t MIN_DATE = 1517097600;
+std::vector<std::vector<Revision>> revisionSnapshots;
 
-std::unordered_map<std::string,uint64_t> buildSnapshotTable(const std::vector<Revision*> &snapshot) {
+std::unordered_map<std::string,uint64_t> buildSnapshotTable(const std::vector<Revision> &snapshot) {
 	std::unordered_map<std::string,uint64_t> result;
-	for(Revision *revision : snapshot) {
-		result[revision->revisionID] = revision->date;
+	for(Revision revision : snapshot) {
+		result[revision.revisionID] = revision.date;
 	}
 	return result;
 }
@@ -86,16 +86,53 @@ std::string uint64_to_string( uint64_t value ) {
     return os.str();
 }
 
-void sortVectorByDate(int start, int end, std::vector<std::vector<Revision*>> &revisionShapshots) {
+void sortVectorByDate(int start, int end) {
 	for(int i = start; i < end; ++i) {
-		std::sort(revisionShapshots[i].begin(), revisionShapshots[i].end(), [](const Revision* lhs, const Revision* rhs){
-			return lhs->date < rhs->date;
+		std::sort(revisionSnapshots[i].begin(), revisionSnapshots[i].end(), [](const Revision &lhs, const Revision &rhs){
+			return lhs.date < rhs.date;
 		});
 	}
 }
+
+void findFork(int start, int end) {
+	std::ofstream myfile;
+	myfile.open("/home/sv/snapshot_fork_date_" + std::to_string(start) + ".csv", std::ofstream::out | std::ofstream::app);
+	auto t_start = std::chrono::high_resolution_clock::now();
+	for(int i = start; i < end; ++i) {
+		auto t_start_build = std::chrono::high_resolution_clock::now();
+		std::unordered_map<std::string, uint64_t> snapshotTable = buildSnapshotTable(revisionSnapshots[i]);
+		std::cout<<"finish build snapshot table with snapshot: "<<snapshots[i]<<" and number: "<<i<<std::endl;
+		auto t_end_build = std::chrono::high_resolution_clock::now();
+		double elapsed_time_ms_build = std::chrono::duration<double, std::milli>(t_end_build-t_start_build).count();
+		elapsed_time_ms_build /= 1000.0;
+		std::cout<<"Time: "<<elapsed_time_ms_build<<" seconds."<<std::endl;
+		for(int j = 0; j < int(revisionSnapshots.size()); ++j) {
+			if(j != i) {
+				for(int k = revisionSnapshots[j].size()-1; k >= 0; --k) {
+					if(snapshotTable.count(revisionSnapshots[j][k].revisionID)) {
+						std::string ret="";
+						ret += uint64_to_string(snapshots[i]);
+						ret += ",";
+						ret += uint64_to_string(snapshots[j]);
+						ret += ",";
+						ret += uint64_to_string(revisionSnapshots[j][k].date);
+						ret += "\n";
+						myfile<<ret;
+						break;
+					}
+				}
+			}
+		}
+	}
+	auto t_end = std::chrono::high_resolution_clock::now();
+	auto elapsed_time_ms = std::chrono::duration<double, std::milli>(t_end-t_start).count();
+	elapsed_time_ms /= 1000.0;
+	std::cout<<"finish writing end at(excluded) "<<end<<": "<<elapsed_time_ms<<" seconds."<<std::endl;
+	myfile.close();
+}
+
 void loadRevisionHistory(std::string name)
 {
-	std::vector<std::vector<Revision*>> revisionSnapshots;
 	int index = 0;
     //Read from the first command line argument, assume it's gzipped
     std::ifstream file(name, std::ios_base::in | std::ios_base::binary);
@@ -132,17 +169,17 @@ void loadRevisionHistory(std::string name)
 			//the first snapshot
 			if(snapshots.size() == 0){
 				snapshots.emplace_back(snapshotID);
-				revisionSnapshots.emplace_back(std::vector<Revision*>{});
-				revisionSnapshots[index].emplace_back(new Revision(revisionID, date));
+				revisionSnapshots.emplace_back(std::vector<Revision>{});
+				revisionSnapshots[index].emplace_back(Revision(revisionID, date));
 				std::cout<<"Snapshot id: "<<snapshotID<<"with number: "<<snapshots.size()<<std::endl;
 			}else {
 				if(snapshotID == snapshots[index]){//old snapshot
-					revisionSnapshots[index].emplace_back(new Revision(revisionID, date));
+					revisionSnapshots[index].emplace_back(Revision(revisionID, date));
 				}else {//new snapshot
 					snapshots.emplace_back(snapshotID);
-					revisionSnapshots.emplace_back(std::vector<Revision*>{});
+					revisionSnapshots.emplace_back(std::vector<Revision>{});
 					++index;
-					revisionSnapshots[index].emplace_back(new Revision(revisionID, date));
+					revisionSnapshots[index].emplace_back(Revision(revisionID, date));
 					std::cout<<"Snapshot id: "<<snapshotID<<"with number: "<<snapshots.size()<<std::endl;
 				}
 			}
@@ -156,58 +193,75 @@ void loadRevisionHistory(std::string name)
 	std::cout<<"finish read: "<<elapsed_time_ms<<" seconds."<<std::endl;
 	//sort date
 	t_start = std::chrono::high_resolution_clock::now();
-	std::vector<std::thread> threads;
-	int start = 0;
-	int end = 60724;
-	const int step = 60724;
 	int len = revisionSnapshots.size();
+	const int step = 60724;
+	int start = 0;
+	int end = step;
+	std::vector<std::thread> threads;
 	while(end < len) {
-		threads.emplace_back(std::thread(sortVectorByDate, start, end, std::ref(revisionSnapshots)));
-		start += step;
-		end = std::min(end + step, len);
+		threads.emplace_back(std::thread(sortVectorByDate, start, end));
+		start = end;
+		end = std::min(end+step, len);
 	}
-	threads.emplace_back(std::thread(sortVectorByDate, revisionSnapshots, start, end, std::ref(revisionSnapshots)));
-	for(auto &th: threads) {
+	threads.emplace_back(std::thread(sortVectorByDate, start, end));
+
+	for (auto &th : threads) {
 		th.join();
 	}
 	t_end = std::chrono::high_resolution_clock::now();
 	elapsed_time_ms = std::chrono::duration<double, std::milli>(t_end-t_start).count();
 	elapsed_time_ms /= 1000.0;
 	std::cout<<"finish sort: "<<elapsed_time_ms<<" seconds."<<std::endl;
-	std::ofstream myfile;
-	myfile.open("/home/sv/snapshot_fork_date.csv", std::ofstream::out | std::ofstream::app);
+
 	t_start = std::chrono::high_resolution_clock::now();
-	for(int i = 0; i < int(revisionSnapshots.size()); ++i) {
-		auto t_start_build = std::chrono::high_resolution_clock::now();
-		std::unordered_map<std::string, uint64_t> snapshotTable = buildSnapshotTable(revisionSnapshots[i]);
-		std::cout<<"finish build snapshot table with snapshot: "<<snapshots[i]<<" and number: "<<i<<std::endl;
-		auto t_end_build = std::chrono::high_resolution_clock::now();
-		double elapsed_time_ms_build = std::chrono::duration<double, std::milli>(t_end_build-t_start_build).count();
-		elapsed_time_ms_build /= 1000.0;
-		std::cout<<"Time: "<<elapsed_time_ms_build<<" seconds."<<std::endl;
-		for(int j = 0; j < int(revisionSnapshots.size()); ++j) {
-			if(j != i) {
-				for(int k = revisionSnapshots[j].size()-1; k >= 0; --k) {
-					if(snapshotTable.count(revisionSnapshots[j][k]->revisionID)) {
-						std::string ret="";
-						ret += uint64_to_string(snapshots[i]);
-						ret += ",";
-						ret += uint64_to_string(snapshots[j]);
-						ret += ",";
-						ret += uint64_to_string(revisionSnapshots[j][k]->date);
-						ret += "\n";
-						myfile<<ret;
-						break;
-					}
-				}
-			}
-		}
+	std::vector<std::thread> fork_threads;
+	start = 0;
+	end = step;
+	while(end < len) {
+		fork_threads.emplace_back(std::thread(findFork, start, end));
+		start = end;
+		end = std::min(end+step, len);
 	}
-	t_end = std::chrono::high_resolution_clock::now();
+	fork_threads.emplace_back(std::thread(findFork, start, end));
+	for (auto &th : fork_threads) {
+		th.join();
+	}
 	elapsed_time_ms = std::chrono::duration<double, std::milli>(t_end-t_start).count();
 	elapsed_time_ms /= 1000.0;
 	std::cout<<"finish writing: "<<elapsed_time_ms<<" seconds."<<std::endl;
-	myfile.close();
+
+	// t_start = std::chrono::high_resolution_clock::now();
+	// for(int i = 0; i < int(revisionSnapshots.size()); ++i) {
+	// 	auto t_start_build = std::chrono::high_resolution_clock::now();
+	// 	std::unordered_map<std::string, uint64_t> snapshotTable = buildSnapshotTable(revisionSnapshots[i]);
+	// 	std::cout<<"finish build snapshot table with snapshot: "<<snapshots[i]<<" and number: "<<i<<std::endl;
+	// 	auto t_end_build = std::chrono::high_resolution_clock::now();
+	// 	double elapsed_time_ms_build = std::chrono::duration<double, std::milli>(t_end_build-t_start_build).count();
+	// 	elapsed_time_ms_build /= 1000.0;
+	// 	std::cout<<"Time: "<<elapsed_time_ms_build<<" seconds."<<std::endl;
+	// 	for(int j = 0; j < int(revisionSnapshots.size()); ++j) {
+	// 		if(j != i) {
+	// 			for(int k = revisionSnapshots[j].size()-1; k >= 0; --k) {
+	// 				if(snapshotTable.count(revisionSnapshots[j][k].revisionID)) {
+	// 					std::string ret="";
+	// 					ret += uint64_to_string(snapshots[i]);
+	// 					ret += ",";
+	// 					ret += uint64_to_string(snapshots[j]);
+	// 					ret += ",";
+	// 					ret += uint64_to_string(revisionSnapshots[j][k].date);
+	// 					ret += "\n";
+	// 					myfile<<ret;
+	// 					break;
+	// 				}
+	// 			}
+	// 		}
+	// 	}
+	// }
+	// t_end = std::chrono::high_resolution_clock::now();
+	// elapsed_time_ms = std::chrono::duration<double, std::milli>(t_end-t_start).count();
+	// elapsed_time_ms /= 1000.0;
+	// std::cout<<"finish writing: "<<elapsed_time_ms<<" seconds."<<std::endl;
+	// myfile.close();
 }
 
 // Revision findForkPositions(const vector<Revision> &original, const vector<Revision> &potentialFork)
