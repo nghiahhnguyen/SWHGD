e6820db0575e537fa77008d5d9cbf68109f83bcc
Nghia <huunghia160799@gmail.com>
2020-07-29 09:42:36 +0700

    Add a fixed end date for all projects and add frequency metrics

--- fork-detection/remove_dup_and_extract_fork_pattern.py
+++ fork-detection/remove_dup_and_extract_fork_pattern.py
@@ -9,7 +9,7 @@ rows_to_keep = []
 snapshot_fork_revision_duplicated_removed = pd.DataFrame(columns=["snapshot_id", "fork_id", "date"])
 row_count = 0
 chunksize = 100000
-for chunk in pd.read_csv('/mnt/17volume/data/snapshot_fork.csv.gz', compression='gzip', names=["date", "fork_id", "snapshot_id",], header=None, chunksize=chunksize):
+for chunk in pd.read_csv('~/snapshot_fork_date.csv', names=["snapshot_id", "fork_id", "date"], header=None, chunksize=chunksize):
     revision_point = chunk
     revision_point = revision_point.reset_index()
     revision_point = revision_point.sort_values(by=['date'], ascending=False)
@@ -31,34 +31,37 @@ for i, snapshot_dict in snapshot_dicts.items():
     for j, (fork_snapshot_row_idx, date) in snapshot_dict.items():
         rows_to_keep.append(fork_snapshot_row_idx)
 print("Number of forks to keep", len(rows_to_keep))
-iter_csv = pd.read_csv('/mnt/17volume/data/snapshot_fork.csv.gz', compression='gzip', header=None, chunksize=chunksize)
+iter_csv = pd.read_csv('/home/sv/snapshot_fork_date.csv', header=None, names=["snapshot_id", "fork_id", "date"], chunksize=chunksize)
+# read the whole file through chunks and 
 df = pd.concat([chunk[chunk.index.isin(rows_to_keep)] for chunk in iter_csv])
 
 
 """extract relevant fork patterns"""
-
-df['year'] = df.apply(lambda row: datetime.utcfromtimestamp(row['date']).year, axis=1)
-df['month'] = df.apply(lambda row: datetime.utcfromtimestamp(row['date']).month, axis=1)
-df['day'] = df.apply(lambda row: datetime.utcfromtimestamp(row['date']).day, axis=1)
-df['week'] = df.apply(lambda row: datetime.utcfromtimestamp(row['date']).isocalendar()[1], axis=1)
+df['year'] = df.apply(lambda row: datetime.fromtimestamp(row['date']).year, axis=1)
+df['month'] = df.apply(lambda row: datetime.fromtimestamp(row['date']).month, axis=1)
+df['day'] = df.apply(lambda row: datetime.fromtimestamp(row['date']).day, axis=1)
+df['week'] = df.apply(lambda row: datetime.fromtimestamp(row['date']).isocalendar()[1], axis=1)
 
 
 snapshot_fork_dict = dict()
 for i, row in df.iterrows():
     if row.snapshot_id not in snapshot_fork_dict:
         snapshot_fork_dict[row.snapshot_id] = dict()
-
+    
+    # if the year is not recorded yet
     if row.year not in snapshot_fork_dict[row.snapshot_id]:
         # count of year, dictitionary for month, dictionary for week number
         snapshot_fork_dict[row.snapshot_id][row.year] = [1, dict(), dict()] 
     else:
         snapshot_fork_dict[row.snapshot_id][row.year][0] += 1
     
+    # if the month is not recorded yet
     if row.month not in snapshot_fork_dict[row.snapshot_id][row.year][1]:
         snapshot_fork_dict[row.snapshot_id][row.year][1][row.month] = 1
     else:
         snapshot_fork_dict[row.snapshot_id][row.year][1][row.month] += 1
     
+    # if the week number is not recorded yet
     if row.week not in snapshot_fork_dict[row.snapshot_id][row.year][2]:
         snapshot_fork_dict[row.snapshot_id][row.year][2][row.week] = 1
     else:
@@ -95,6 +98,8 @@ def is_steady(units, unit_per_year=12):
         num_iterations -= 1
     return True
 
+print("The number of snapshot initially:", len(snapshot_fork_dict.keys()))
+
 snapshot_ids = []
 count_forks = []
 count_years_exist = [] # the number of years between the first and last forks
@@ -105,9 +110,10 @@ mean_forks_per_month = []
 mean_forks_per_week = []
 count_years_with_forks = []
 count_months_with_forks = []
-count_week_with_forks = []
+count_weeks_with_forks = []
 steady_month = []
 steady_week = []
+frequencies = []
 for snapshot, revision_date_set in snapshot_fork_dict.items():
     forks_year = []
     forks_month = []
@@ -133,11 +139,22 @@ for snapshot, revision_date_set in snapshot_fork_dict.items():
         count_fork += frequency
         count_years_with_fork += 1
     for year, month, freq in forks_month:
+        if year > 2019 or (year == 2019 and month > 1):
+            print(year, month)
+            continue
         months.append((year, month))
         count_months_with_fork += 1
     for year, week, freq in forks_week:
+        if year > 2019 or (year == 2019 and week > 4):
+            print(year, week)
+            continue
         weeks.append((year, week))
         count_weeks_with_fork += 1
+    if len(months) == 0:
+        print("This snapshot has no viable fork date:", snapshot)
+        continue
+    months.append((2019, 1))
+    weeks.append((2019, 4))
     
     months_exist = count_time_units_exists(months, 12)
     weeks_exist = count_time_units_exists(weeks, 53)
@@ -146,6 +163,7 @@ for snapshot, revision_date_set in snapshot_fork_dict.items():
     count_forks.append(count_fork)
     count_years_with_forks.append(count_years_with_fork)
     count_months_with_forks.append(count_months_with_fork)
+    count_weeks_with_forks.append(count_weeks_with_fork)
     years_exist = years[-1] - years[0] + 1
     count_years_exist.append(years_exist)
     count_months_exist.append(months_exist)
@@ -153,6 +171,7 @@ for snapshot, revision_date_set in snapshot_fork_dict.items():
     mean_forks_per_year.append(count_fork / years_exist)
     mean_forks_per_month.append(count_fork / months_exist)
     mean_forks_per_week.append(count_fork / weeks_exist)
+    frequencies.append(count_months_with_fork / months_exist)
     if is_steady(months, 12):
         steady_month.append(1)
     else:
@@ -162,22 +181,24 @@ for snapshot, revision_date_set in snapshot_fork_dict.items():
     else:
         steady_week.append(0)
 
+print("The number of snapshot after:", len(snapshot_ids))
+
 df_fork_features = pd.DataFrame.from_dict({
     "snapshot_id": snapshot_ids,
     "count_forks": count_forks,
-    "mean_forks_per_year": mean_forks_per_year,
     "mean_forks_per_month": mean_forks_per_month,
     "mean_forks_per_week": mean_forks_per_week,
-    "count_years_with_forks": count_years_with_forks,
     "count_months_with_forks": count_months_with_forks,
-    "count_years_with_forks": count_years_exist,
-    "count_months_with_forks": count_months_exist,
-    "count_weeks_with_fork": count_weeks_exist,
+    "count_weeks_with_forks": count_weeks_with_forks,
+    "count_months_exist": count_months_exist,
+    "count_weeks_exist": count_weeks_exist,
     "steady_month": steady_month,
-    "steady_week": steady_week
+    "steady_week": steady_week,
+    "frequencies": frequencies
     
 }, orient='index').transpose()
 
-print(df_fork_features.head(16))
+print(df_fork_features.head(10))
 
-df_fork_features.to_csv("/home/sv/data/fork_pattern.csv")
+df_fork_features.to_csv("/home/sv/fork_pattern.csv")
+df_fork_features[["snapshot_id"]].drop_duplicates().to_csv("/home/sv/snapshot_id.csv")
